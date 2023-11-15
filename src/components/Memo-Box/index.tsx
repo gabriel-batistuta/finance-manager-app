45b2379762beb4c2872aaae562f2a266d0f65013
Gabriel <batistutag190@gmail.com>
2023-11-15 15:04:14 -0300

    criado projeto de app gerenciador financeiro

---
+++ src/components/Memo-Box/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function MemoBox() {
+
+    //caixinha de contas a pagar
+    function openWidget() {
+
+    }
+
+    return(
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.buttonBox} onPress={()=> {}}>
+
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }, buttonBox: {
+
+    }
+})
